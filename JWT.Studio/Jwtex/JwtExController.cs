0f3f29c1fd8c9892095ef9fc31b9771e7226fbb8
JUkhan <jasim.uddin.khan@gmail.com>
2015-01-24 22:12:03 +0600

    integrate codemirror

---
+++ JWT.Studio/Jwtex/JwtExController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Jwtex
+{
+    public class JwtExController:Controller
+    {
+        public void Index()
+        {
+            string nameSpace = GetType().Assembly.GetName().Name;
+            string index = "Jwtex.JwtResources.index.html";
+            Response.ContentType = "text/html";
+            using (Stream stream = GetType().Assembly.GetManifestResourceStream(index))
+            {
+                stream.CopyTo(Response.OutputStream);
+            }
+
+        }
+        public void Resources(string file)
+        {
+            string ext = file.Substring(file.LastIndexOf(".") + 1);
+
+            using (Stream stream = GetType().Assembly.GetManifestResourceStream("Jwtex.JwtResources." + file))
+            {
+                switch (ext)
+                {
+                    case "html":
+                        Response.ContentType = "text/html";
+                        break;
+                    case "css":
+                        Response.ContentType = "text/css";
+                        break;
+                    case "js":
+                        Response.ContentType = "text/javascript";
+                        break;
+                }
+                stream.CopyTo(Response.OutputStream);
+            }
+
+        }
+    }
+}
---
+++ JWT.Studio/Jwtex/JwtResources/mainController.js
@@ -0,0 +1,7 @@
+﻿
+angular.module('app').controller('mainController', ['$scope', '$http', '$modal', function (scope, http, modal) {
+   
+    scope.msg = 'Main controller message...';
+    scope.editorContent = "angular.module('app').controller('mainCtrl', ['$scope', '$sce', app.controllers.mainCtrl]);";
+}]);
+
