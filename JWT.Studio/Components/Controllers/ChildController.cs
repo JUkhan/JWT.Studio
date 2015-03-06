38e55f55df697ed3062bb616ca5a76a510e9eae6
unknown <Jasim.Uddin@DACW0066.internal.imsglobal.com>
2015-03-06 11:06:59 +0600

    add component project

---
+++ JWT.Studio/Components/Controllers/ChildController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Components.Controllers
+{
+    public class ChildController : Controller
+    {
+        //
+        // GET: /Child/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public void GetComponent(string componentName)
+        {
+            string SOURCE = Server.MapPath("~") + "Components/" + componentName;
+            string DESTINATION = Server.MapPath("~") + componentName + ".zip";
+            if (System.IO.File.Exists(DESTINATION))
+            {
+                System.IO.File.Delete(DESTINATION);
+            }
+            ZipFile.CreateFromDirectory(SOURCE, DESTINATION);
+            Response.TransmitFile(DESTINATION);
+
+        }
+
+    }
+}
---
+++ JWT.Studio/Jwtex/JwtComponentController.cs
@@ -0,0 +1,84 @@
+﻿using Jwt.Controller;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+namespace Jwtex
+{
+    public class JwtComponentController : BaseController
+    {
+        public void Index()
+        {
+            string nameSpace = GetType().Assembly.GetName().Name;
+            string index = "Jwtex.JwtResources.component.html";
+            Response.ContentType = "text/html";
+            using (Stream stream = GetType().Assembly.GetManifestResourceStream(index))
+            {
+                stream.CopyTo(Response.OutputStream);
+            }
+
+        }
+
+        public JsonResult Download()
+        {
+            try
+            {
+                using (MemoryStream mem = GetURLContents("http://localhost:29324/Child/GetComponent?componentName=init"))
+                {
+                    using (ZipArchive arc = new ZipArchive(mem))
+                    {
+                        var path = Config.Root +"Scripts//Directives//init";
+                        if (Directory.Exists(path))
+                        {
+                            RemoveDirectoryFiles(path);
+                        }
+                        arc.ExtractToDirectory(path);
+                    }
+                }
+                return Json(new { msg = "installed successfully" }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { msg = ex.ToString() }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+
+        private void RemoveDirectoryFiles(string path)
+        {
+            DirectoryInfo dir = new DirectoryInfo(path);
+            foreach (FileInfo file in dir.GetFiles()) file.Delete();
+        }
+        private MemoryStream GetURLContents(string url)
+        {
+            // The downloaded resource ends up in the variable named content. 
+            var content = new MemoryStream();
+
+            // Initialize an HttpWebRequest for the current URL. 
+            var webReq = (HttpWebRequest)WebRequest.Create(url);
+
+            // Send the request to the Internet resource and wait for 
+            // the response. 
+            // Note: you can't use HttpWebRequest.GetResponse in a Windows Store app. 
+            using (WebResponse response = webReq.GetResponse())
+            {
+
+                // Get the data stream that is associated with the specified URL. 
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    // Read the bytes in responseStream and copy them to content.  
+                    responseStream.CopyTo(content);
+                }
+            }
+
+            // Return the result as a byte array. 
+            return content;
+        }
+    
+    }
+}
